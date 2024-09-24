a84336e4199bad21a5c1eb4cbcdf105c1a870d83
Surachach <surachat@praneat.com>
2024-09-24 23:42:08 +0700

    Hello World

---
+++ src/app/[locale]/(auth)/home/page.tsx
@@ -0,0 +1,21 @@
+const Home = () => {
+  return (
+
+    <div className="h-screen w-screen bg-red-400">
+      <div className="h-[240px] w-full px-10">
+
+      </div>
+      <div className="flex justify-between">
+        <div>หน้าแรก</div>
+        <div>My Feed</div>
+        <div>Pantip Pick</div>
+        <div>Pantip Hitz</div>
+        <div>Explore</div>
+      </div>
+
+    </div>
+
+  );
+};
+
+export default Home;
